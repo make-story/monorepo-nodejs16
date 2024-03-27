4f71236e6e2e61377e903899878afc92697f6f4b
[상품상세개발팀] 유성민 <>
2024-03-27 16:04:28 +0900

    update

--- apps/vuejs-test/src/basics/components/test/Presentational.vue
+++ apps/vuejs-test/src/basics/components/test/Presentational.vue
@@ -61,6 +61,11 @@ export default {
       this.onClick('컨테이너야 메시지(데이터) 받아라!');
     },
     // props 로 받은 객체타입 데이터 변경시, 상위 컴포넌트에도 반영되는지 여부? 변경된다!
+    /**
+     * [경고!!!!] 하위 컴포넌트 등에서 item (객체타입) 값을 변경해서는 안된다!
+     * item.xxx.yyy = true; 형태로 값을 변경할 경우, item 을 참조하는 다른 컴포넌트에 영향이 있다! 즉, 디버깅 또는 watch 등이 있을 경우 순환참조에 빠질 수 있다!
+     * props 로 받은 item 객체타입을 data 에 원시타입형태로 바인딩(data() { return { isTest: this.item.isTest }; }) 하고, 해당 값을 사용하는 것이 좋다!
+     */
     onClickContainerDataChange() {
       this.item.count = this.item.count + 1;
     },
